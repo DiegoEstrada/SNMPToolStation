7a128e57d563a4c4d9f0f4faf308bf11c43cceb5
DiegoEstrada <diegoestrada97@hotmail.com>
2019-02-23 21:37:40 -0600

    Setting up dependencies

---
+++ examen/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/diego/Documents/Administracion de Servicios en Red/Examen1/examen/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
